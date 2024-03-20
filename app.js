da032727b9fac0d60c44dda74efa289f2ce1a6ba
Jyoti Prakash Behera <jyotiprakshb23@gmail.com>
2024-03-20 15:58:57 +0530

    cors headers added

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const products_routes = require("./routes/products");
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+//middleware to enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+//middleware to use the routes
 app.use("/api/products", products_routes);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
